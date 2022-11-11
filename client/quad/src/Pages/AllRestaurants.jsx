2817c95dab7ab58a8d2f9b3684be52b8dc4e4887
Shubham0442 <shubhamrakhonde507@gmail.com>
2022-11-11 13:10:12 +0530

    AllRestaurants page added also allrestaurant route added

---
+++ client/quad/src/Pages/AllRestaurants.jsx
@@ -0,0 +1,16 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+const AllRestaurants = () => {
+  return (
+    <Box>
+        <Navbar/>
+        <Box>
+            
+        </Box>
+    </Box>
+  )
+}
+
+export default AllRestaurants
