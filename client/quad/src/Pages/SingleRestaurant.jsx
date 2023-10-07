bc6f79a95c2295809e7d798e9a783fce9d5e4715
Shubham Rakhonde <shubhamrakhonde507@gmail.com>
2023-10-07 14:05:32 +0530

    added toaster if no token, footer content font size reduced as per screen

--- client/quad/src/Pages/SingleRestaurant.jsx
+++ client/quad/src/Pages/SingleRestaurant.jsx
@@ -8,9 +8,9 @@ import {
   addToCart,
   getCart,
   singleRestaurant,
-  singleRestaurantName,
+  singleRestaurantName
 } from "../Redux/Reducers/RestaurantReducer/action";
-import { FaStar, FaYenSign } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
 
 const SingleRestaurant = () => {
   const dispatch = useDispatch();
@@ -19,7 +19,9 @@ const SingleRestaurant = () => {
   const [singleRestaurantData, setSingleRestaurantData] = useState({});
   const token = useSelector((state) => state.userReducer.token);
   const { restId } = useParams();
-  console.log(restId);
+  const noUserLoginToast = useToast();
+  const itemAddToCartToast = useToast();
+  console.log(restId, token);
 
   useEffect(() => {
     dispatch(singleRestaurant(restId)).then((res) => {
@@ -45,14 +47,13 @@ const SingleRestaurant = () => {
         AddTOCartToast({
           title: "Item Added to cart",
           status: "success",
-          duration: 1500,
-          position: "top",
+          duration: 3000,
+          position: "top-right"
         });
         dispatch(getCart(token));
       }
     });
   };
-
 
   const cuisine = singleRestaurantData?.cuisines?.join(", ");
 
@@ -66,104 +67,150 @@ const SingleRestaurant = () => {
           flexDirection={{
             base: "column-reverse",
             sm: "column-reverse",
-            md: "row",
+            md: "row"
           }}
-          gap={{base: "20px", sm: "20px", md: "40px", lg: "80px"}}
+          gap={{ base: "20px", sm: "20px", md: "40px", lg: "80px" }}
           p="50px 30px"
           color="white"
-          w={{base: "100%", sm: "100%", md: "100%", lg: "80%" }}
+          w={{ base: "100%", sm: "100%", md: "100%", lg: "80%" }}
           m="auto"
           justifyContent={"space-evenly"}
         >
-          <Box w={{ base: "100%", sm: "100%", md: "50%", lg: "60%" }} p="20px" m={"auto"} >
-            <Box w={{base: "100%", sm: "70%", md: "100%", lg: "100%"}} m={"auto"}>
-            <Text fontSize={"32px"} alignItems={"center"} fontWeight="400" textAlign={"left"}>
-              {singleRestaurantData.rest_name}
-            </Text>
-            <Text
-              color={"#b1b2b7"}
-              fontSize="15px"
-              mt="10px"
-              textAlign="left"
-              height="20px"
-              w="80%"
-            >
-              {cuisine}
-            </Text>
-
-            <Text
-              color={"#b1b2b7"}
-              fontSize="15px"
-              mt={{ base: "25px", sm: "25px", md: "25px", lg: "10px" }}
-              textAlign={"left"}
-            >
-              {singleRestaurantData.address}
-            </Text>
-
+          <Box
+            w={{ base: "100%", sm: "100%", md: "50%", lg: "60%" }}
+            p="20px"
+            m={"auto"}
+          >
             <Box
-              display={"flex"}
-              mt="20px"
-              gap={{ md: "30px", lg: "50px" }}
-              textAlign="left"
-              alignItems={"left"}
-              pt="15px"
-              w="100%"
+              w={{ base: "100%", sm: "70%", md: "100%", lg: "100%" }}
+              m={"auto"}
             >
-              <Box>
-                <Box
-                  display={"flex"}
-                  justifyContent={"space-evenly"}
-                  gap="5px"
-                  fontWeight={"600"}
-                  p="0px 5px"
-                  fontSize={"16px"}
-                  alignItems={"center"}
-                  color="white"
-                >
-                  <Box>
-                    <FaStar fontSize={"12px"} />
+              <Text
+                fontSize={"32px"}
+                alignItems={"center"}
+                fontWeight="400"
+                textAlign={"left"}
+              >
+                {singleRestaurantData.rest_name}
+              </Text>
+              <Text
+                color={"#b1b2b7"}
+                fontSize="15px"
+                mt="10px"
+                textAlign="left"
+                height="20px"
+                w="80%"
+              >
+                {cuisine}
+              </Text>
+
+              <Text
+                color={"#b1b2b7"}
+                fontSize="15px"
+                mt={{ base: "25px", sm: "25px", md: "25px", lg: "10px" }}
+                textAlign={"left"}
+              >
+                {singleRestaurantData.address}
+              </Text>
+
+              <Box
+                display={"flex"}
+                mt="20px"
+                gap={{ md: "30px", lg: "50px" }}
+                textAlign="left"
+                alignItems={"left"}
+                pt="15px"
+                w="100%"
+              >
+                <Box>
+                  <Box
+                    display={"flex"}
+                    justifyContent={"space-evenly"}
+                    gap="5px"
+                    fontWeight={"600"}
+                    p="0px 5px"
+                    fontSize={"16px"}
+                    alignItems={"center"}
+                    color="white"
+                  >
+                    <Box>
+                      <FaStar fontSize={"12px"} />
+                    </Box>
+                    <Box>
+                      <Text>{singleRestaurantData.rating}</Text>
+                    </Box>
                   </Box>
-                  <Box>
-                    <Text>{singleRestaurantData.rating}</Text>
-                  </Box>
+
+                  <Text
+                    color={"#b1b2b7"}
+                    alignItems={"center"}
+                    fontSize="12px"
+                    mt="2px"
+                  >
+                    Ratings
+                  </Text>
                 </Box>
 
-                <Text color={"#b1b2b7"} alignItems={"center"} fontSize="12px" mt="2px">
-                  Ratings
-                </Text>
-              </Box>
+                <Box
+                  borderLeft="1px solid #b1b2b7"
+                  height="40px"
+                  ml={"10px"}
+                  mr={"10px"}
+                ></Box>
 
-              <Box borderLeft="1px solid #b1b2b7" height="40px" ml={"10px"} mr={"10px"}></Box>
+                <Box>
+                  <Text
+                    fontWeight={"600"}
+                    fontSize={{ base: "", sm: "", md: "14px", lg: "16px" }}
+                  >{`${singleRestaurantData.d_time} MINS`}</Text>
 
-              <Box>
-                <Text
-                  fontWeight={"600"}
-                  fontSize={{ base: "", sm: "", md: "14px", lg: "16px" }}
-                >{`${singleRestaurantData.d_time} MINS`}</Text>
+                  <Text
+                    color={"#b1b2b7"}
+                    alignItems={"center"}
+                    fontSize="12px"
+                    mt="2px"
+                  >
+                    Delivery Time
+                  </Text>
+                </Box>
+                <Box
+                  borderLeft="1px solid #b1b2b7"
+                  height="40px"
+                  ml={"10px"}
+                  mr={"10px"}
+                ></Box>
 
-                <Text color={"#b1b2b7"} alignItems={"center"} fontSize="12px" mt="2px">
-                  Delivery Time
-                </Text>
-              </Box>
-              <Box borderLeft="1px solid #b1b2b7" height="40px" ml={"10px"} mr={"10px"}></Box>
-
-              <Box>
-                <Text>{`₹${singleRestaurantData.cost}`}</Text>
-                <Text color={"#b1b2b7"} alignItems={"center"} fontSize="12px" mt="2px">
-                  Cost for two
-                </Text>
+                <Box>
+                  <Text>{`₹${singleRestaurantData.cost}`}</Text>
+                  <Text
+                    color={"#b1b2b7"}
+                    alignItems={"center"}
+                    fontSize="12px"
+                    mt="2px"
+                  >
+                    Cost for two
+                  </Text>
+                </Box>
               </Box>
             </Box>
-            </Box> 
           </Box>
-          <Box w={{ base: "100%", sm: "100%", md: "40%", lg: "40%" }} p="20px"  m={"auto"}>
-            <Image src={singleRestaurantData.image_rest} height="200px" w={{base: "100%", sm: "70%", md: "100%", lg: "100%"}} m={"auto"}/>
+          <Box
+            w={{ base: "100%", sm: "100%", md: "40%", lg: "40%" }}
+            p="20px"
+            m={"auto"}
+          >
+            <Image
+              src={singleRestaurantData.image_rest}
+              height="200px"
+              w={{ base: "100%", sm: "70%", md: "100%", lg: "100%" }}
+              m={"auto"}
+            />
           </Box>
         </Box>
       </Box>
 
       <Box
-        w={{base: "90%", sm: "80%", md: "65%", lg:"50%"}}
+        w={{ base: "90%", sm: "80%", md: "65%", lg: "50%" }}
         m="auto"
         h="600px"
         overflowY="scroll"
@@ -171,17 +218,17 @@ const SingleRestaurant = () => {
         mb="30px"
         css={{
           "&::-webkit-scrollbar": {
-            width: "4px",
+            width: "4px"
           },
           "&::-webkit-scrollbar-track": {
-            width: "6px",
+            width: "6px"
           },
           "&::-webkit-scrollbar-thumb": {
             background: "#fff",
-            borderRadius: "24px",
-          },
+            borderRadius: "24px"
+          }
         }}
-       >
+      >
         {menu.map((item) => (
           <Box key={item._id} p="20px" position={"relative"}>
             <Box
@@ -201,12 +248,11 @@ const SingleRestaurant = () => {
                   mt="15px"
                   overflow="hidden"
                   color="#9ea0a8"
-                  fontSize={{ base:"12px", sm: "12.5px", md: "14px"}}
+                  fontSize={{ base: "12px", sm: "12.5px", md: "14px" }}
                 >
                   {item.description}
                 </Text>
               </Box>
-
               <Box w="20%">
                 <Image
                   src={item.item_image}
@@ -225,7 +271,18 @@ const SingleRestaurant = () => {
                 borderRadius={"5px"}
                 alignItems="center"
                 color={"white"}
-                onClick={() => handleAddToCart(item._id)}
+                onClick={() => {
+                  if (token) handleAddToCart(item._id);
+                  else
+                    noUserLoginToast({
+                      title: "Fail to Add in cart!",
+                      description: "Please Signup or login!",
+                      status: "error",
+                      duration: 4000,
+                      isClosable: true,
+                      position: "top-right"
+                    });
+                }}
               >
                 Add
               </Button>
