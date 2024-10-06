db474d8e8c851e155cd938252734c17b1cee84b7
Shubham Rakhonde <shubhamrakhonde507@gmail.com>
2024-10-06 20:17:34 +0530

    added the loader on first request to servr

--- client/quad/src/Redux/Reducers/RestaurantReducer/restaurantReducer.js
+++ client/quad/src/Redux/Reducers/RestaurantReducer/restaurantReducer.js
@@ -6,14 +6,18 @@ import {
   GET_RESTAURANTS_FAILURE,
   GET_RESTAURANTS_REQUEST,
   GET_RESTAURANTS_SUCCESS,
+  INITIAL_GET_RESTAURANTS_BY_CITY_FAILURE,
+  INITIAL_GET_RESTAURANTS_BY_CITY_REQUEST,
+  INITIAL_GET_RESTAURANTS_BY_CITY_SUCCESS
 } from "./actionTypes";
 
 const initState = {
   allRestaurantsByCity: [],
-  allRestaurants:[],
-  cart:[],
+  allRestaurants: [],
+  cart: [],
   isLoading: false,
   isError: false,
+  initialLoading: false
 };
 
 export const restaurantReducer = (state = initState, { type, payload }) => {
@@ -22,6 +26,7 @@ export const restaurantReducer = (state = initState, { type, payload }) => {
       return {
         ...state,
         isLoading: true,
+        initialLoading: false
       };
     }
     case GET_RESTAURANTS_BY_CITY_SUCCESS: {
@@ -29,6 +34,7 @@ export const restaurantReducer = (state = initState, { type, payload }) => {
         ...state,
         isLoading: false,
         allRestaurantsByCity: payload,
+        initialLoading: false
       };
     }
 
@@ -37,14 +43,15 @@ export const restaurantReducer = (state = initState, { type, payload }) => {
         ...state,
         isLoading: false,
         isError: true,
+        initialLoading: false
       };
     }
 
-    
     case GET_RESTAURANTS_REQUEST: {
       return {
         ...state,
         isLoading: true,
+        initialLoading: false
       };
     }
 
@@ -53,6 +60,7 @@ export const restaurantReducer = (state = initState, { type, payload }) => {
         ...state,
         isLoading: false,
         allRestaurants: payload,
+        initialLoading: false
       };
     }
 
@@ -61,6 +69,7 @@ export const restaurantReducer = (state = initState, { type, payload }) => {
         ...state,
         isLoading: false,
         isError: true,
+        initialLoading: false
       };
     }
 
@@ -69,9 +78,27 @@ export const restaurantReducer = (state = initState, { type, payload }) => {
         ...state,
         isLoading: false,
         cart: payload,
+        initialLoading: false
       };
     }
-    
+    case INITIAL_GET_RESTAURANTS_BY_CITY_REQUEST:
+      return {
+        ...state,
+        isLoading: false,
+        initialLoading: true
+      };
+    case INITIAL_GET_RESTAURANTS_BY_CITY_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        initialLoading: false
+      };
+    case INITIAL_GET_RESTAURANTS_BY_CITY_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        initialLoading: false
+      };
     default:
       return state;
   }
